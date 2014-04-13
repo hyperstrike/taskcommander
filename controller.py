82c7b7fca7c01eb02fd727c162d27a2d87a593b7
laurenmoos <lauren.a.moos@gmail.com>
2014-04-12 19:19:29 -0700

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,61 @@
+import model
+import modelutilities
+import view 
+
+#as of now we have only worked on communication from view -> model, need to set up communication from model ->view
+
+users = users("Session One")
+#instantiate users and pass userid 
+class CreateAccount:
+	def __init__(self):
+		self.firstname = #field from view with First Name
+		self.lastname = #field from view with Last Name
+		self.userID#field from view with ID Number
+		#need to add something in view with accessStatus 
+		self.birthdate = #field from view with Date of Birth
+	def addUser(self): #initializes User 
+		users.addUser(firstname, lastname, userID, birthdate))
+
+class Login: 
+	def __init__(self):
+		self.username = #field from view with username 
+		self.password = #field  from view with password 
+		self.userID = #field from view with user identification 
+		self.hashedpassword = users[userID].getpassword()
+
+	def validate_password(self):
+		check_password(self.hashedpassword, self.userpassword)
+		#if true have view navigate to main menu, if false have view say invalid 
+
+class CreateProject: #needs user ID from Login
+	def __init__(self):
+		self.name = #field from view with Project Name Entered 
+		self.description = #field from view with Project Description Entered
+		self.startdate = #field from view with Start Date Entered
+		self.enddate = #field from view with End Date Entered
+		self.usercanview = #will have to do something with this so 
+	def CreateProject:
+		CreatedProject = users[Login.userID].addProject(self.name)
+		CreatedProject.setdescription(self.description)
+		CreatedProject.setprojectmanager(User.userID)
+		CreatedProject.setstardate(self.startdate)
+		CreatedProject.setenddate(self.enddate)
+
+class SelectProject: #all going to be information from model -> view
+	def __init__(self):
+		self.projects = users[Login.userID].projects.keys()
+		self.selectedproject = #information from view on which project was selected, get information from model on that project
+	def DisplayProjectNames:
+		for i in range (0, len(self.projects)):
+			#print project name to something in view
+	
+
+
+class EditProject: #all going to be information from model -> view
+
+class CreateTask: #needs user ID from Login and Project Name from CreateProject
+	def __init__(self):
+		self.name = #field from view with Task Name entered
+	def CreateTask:
+		CreatedTask = users[Login.userID].projects[CreateProject.name]
+		Users[CreateAccount.userID].projects[CreateProject.name].addTask(self.name)
